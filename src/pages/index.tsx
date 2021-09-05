40ea3a863369e991454b8f53e5dfc64f98f2c973
Caetano Burjack <caetano.burjack@gmail.com>
2021-09-05 10:51:39 -0300

    Creation of the first page with three components, header, loginButton and subscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 
-// import styles from '../styles/home.module.scss'
+import styles from './home.module.scss'
 import Head from 'next/head'
+import React from 'react'
+import { SubscribeButton } from '../components/SubscribeButton'
 
 export default function Home() {
   return (
@@ -8,7 +10,25 @@ export default function Home() {
       <Head>
         <title>Quick News</title>
       </Head>
-      <h2>Hello <span>World!</span></h2>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>Hey, welcome!</span>
+
+          <h1>👏 News about the <span>React</span> world.</h1>
+
+          <p>
+            Get access to all the publications <br />
+            <span>for $ 9.90 monthly</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+
+      </main>
+
+
     </>
   )
 }
