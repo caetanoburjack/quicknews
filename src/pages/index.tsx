4efbc24d3162cebe574021a69b0fca858221b6ed
Caetano Burjack <caetano.burjack@gmail.com>
2021-09-06 15:03:19 -0300

    Set up of the Stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
+import { GetStaticProps } from 'next'
 
-import styles from './home.module.scss'
 import Head from 'next/head'
 import React from 'react'
-import { SubscribeButton } from '../components/SubscribeButton'
 
-export default function Home() {
+import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
+
+import styles from './home.module.scss'
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+  console.log(product)
   return (
     <>
       <Head>
@@ -19,9 +31,9 @@ export default function Home() {
 
           <p>
             Get access to all the publications <br />
-            <span>for $ 9.90 monthly</span>
+            <span>for {product.amount} monthly</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
@@ -31,4 +43,23 @@ export default function Home() {
 
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JWM5AF6Sk7RiJrEYsYYHo8Z')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24,//24 horas
+  }
 }
