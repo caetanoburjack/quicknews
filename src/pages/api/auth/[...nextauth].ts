6eef0c6b89d9b37b85950056a2521e63c001c59e
Caetano Burjack <caetano.burjack@gmail.com>
2022-05-12 09:15:19 -0300

    setting up strict to false on tsconfig

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,14 +4,12 @@ import { query as q } from 'faunadb'
 import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
-    // Configure one or more authentication providers
     providers: [
         Providers.GitHub({
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
             scope: 'read:user'
         }),
-        // ...add more providers here
     ],
     callbacks: {
         async session(session) {
